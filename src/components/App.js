186e930485aa9e3240cf264dfaaddd0d90b2d5a7
Arthur Ish <arthurishdev@gmail.com>
2025-01-13 23:57:28 -0800

    dogs are fetched!

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [dog, setDog] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => { setDog(data.message) })
+        .then(() => setIsLoaded(true));
+    }, []);
+
+    if(isLoaded === false){
+        return (
+            <p>Loading</p>
+        );
+    }
+    return (
+        <img alt="A Random Dog" src={dog}></img>
+    )
+}
+
+export default App;
